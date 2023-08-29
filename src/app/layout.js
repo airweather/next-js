8dc2c61172ec7768da26bbaa9fd80cdc8cca7693
Jun <wnsghd1@naver.com>
2023-08-29 22:38:43 +0900

    layout.js page.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,4 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +7,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ol>
+          <li><a href="/read/1">html</a></li>
+          <li><a href="/read/2">css</a></li>
+        </ol>
+          {children}
+          <ul>
+            <li><a href="/create">create</a></li>
+            <li><a href="/update">update</a></li>
+            <li><input type="button" value="delete"/></li>
+          </ul>
+      </body>
     </html>
   )
 }
