04565ee44e4e4d3f1362c9139c8171e142ee0947
Jun <wnsghd1@naver.com>
2023-09-02 05:01:27 +0900

    create

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,19 @@
+"use client"
+
 export default function Create () {
   return (
-    <>
-      Create!
-    </>
+    <form onSubmit={()=>{
+      
+    }}>
+      <p>
+        <input type="text" name="title" placeholder="title" />
+      </p>
+      <p>
+        <textarea name="body" placeholder="body"></textarea>
+      </p>
+      <p>
+        <input type="submit" value="create" />
+      </p>
+    </form>
   )
 }
