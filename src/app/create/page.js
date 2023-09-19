578b00f7b1bdd5b5883748c74e071c1a27d75406
Jun <wnsghd1@naver.com>
2023-09-19 23:10:41 +0900

    fetch

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,19 +1,38 @@
 "use client"
 
+import { useRouter } from "next/navigation";
+
 export default function Create () {
+  const router = useRouter();
   return (
-    <form onSubmit={()=>{
-      
-    }}>
-      <p>
-        <input type="text" name="title" placeholder="title" />
-      </p>
-      <p>
-        <textarea name="body" placeholder="body"></textarea>
-      </p>
-      <p>
-        <input type="submit" value="create" />
-      </p>
+    <form onSubmit={(e)=>{
+      e.preventDefault();
+      const title = e.target.title.value;
+      const body = e.target.body.value;
+      const option = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({title, body})
+      }
+
+      fetch('http://localhost:9900/topics', option).then(res => res.json()).then(data => {
+        console.log(data)
+        const lastId = data.id;
+        router.push(`/read/${lastId}`);
+    }>
+      <div>
+        <p>
+          <input type="text" name="title" placeholder="title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="create" />
+        </p>
+      </div>
     </form>
   )
 }
