2a1178e3be88f5ea314ccc47453198d502bd1c47
Jun <wnsghd1@naver.com>
2023-09-20 23:49:54 +0900

    cache no store

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export default async function RootLayout({ children }) {
   // const resp = await fetch('http://localhost:9900/topics', { next: { revalidate: 0 } });
   
   // no-cache
-  const resp = await fetch('http://localhost:9900/topics', { cache: 'no-cache' });
+  const resp = await fetch('http://localhost:9900/topics', { cache: 'no-store' });
   const topics = await resp.json();
 
   return (
